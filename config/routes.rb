e38eefa62a5e32eff924022ac85cca2b40e7e53b
shibatadaisuke <shibata.daisuke15@gmail.com>
2017-08-13 16:01:54 +0900

    routesを設定して、controllerとviewの枠だけ用意した

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # 管理者が記事を作成するために必要なセクションと記事を用意
+  namespace :admin do
+    resources :sections do
+      resources :articles do
+      end
+    end
+  end
+
 end
